e74a3edad826604224c9f40df334c9ac5733cb8c
Cristian <cristianav22@gmail.com>
2026-04-25 20:01:15 -0300

    fix vite config para vercel

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,11 @@ export default defineConfig(({ mode }) => {
         'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
         'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
       },
+      build: {
+        rollupOptions: {
+          external: [],
+        }
+      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
